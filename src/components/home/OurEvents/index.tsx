aa74f5150733ea32b5758ecc40a6ec09d7d59800
TinoMaster <oscarmarcos2673@gmail.com>
2023-10-03 17:30:45 +0200

    ok

--- src/components/home/OurEvents/index.tsx
+++ src/components/home/OurEvents/index.tsx
@@ -3,32 +3,17 @@ import e from "../../../../public/assets/images/Salsa.jpg";
 
 export const OurEvents = () => {
   return (
-    <div className="bg-[url('/assets/images/Salsa.jpg')] relative">
-      <h1 className="">OurEvents</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
-        odio veniam non impedit necessitatibus repellendus quaerat dignissimos
-        ipsum unde, placeat facilis, vitae est consectetur quae atque quia
-        aperiam quod minima!
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
-        odio veniam non impedit necessitatibus repellendus quaerat dignissimos
-        ipsum unde, placeat facilis, vitae est consectetur quae atque quia
-        aperiam quod minima!
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
-        odio veniam non impedit necessitatibus repellendus quaerat dignissimos
-        ipsum unde, placeat facilis, vitae est consectetur quae atque quia
-        aperiam quod minima!
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
-        odio veniam non impedit necessitatibus repellendus quaerat dignissimos
-        ipsum unde, placeat facilis, vitae est consectetur quae atque quia
-        aperiam quod minima!
-      </p>
+    <div className="bg-[url('/assets/images/Salsa.jpg')] bg-center bg-cover relative min-h-[1000px]">
+      <div className="absolute inset-0 bg-gradient-to-r from-primary/90 via-primaryLight/90 to-primary/90"></div>
+      <div className="relative z-10">
+        <h1 className="">OurEvents</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+          aperiam debitis iusto aspernatur error eveniet totam aut aliquid,
+          dolorem quasi laudantium beatae cumque similique in. Accusamus ipsa
+          numquam aspernatur voluptates.
+        </p>
+      </div>
     </div>
   );
 };
